d8405cc2adfb61428dcd2e628056fa29b7673884
Ceddyp19 <ceddyp117@gmail.com>
2020-07-30 13:15:27 -0500

    updated games table

--- db/migrate/002_create_games.rb
+++ db/migrate/002_create_games.rb
@@ -12,6 +12,26 @@ class CreateGames < ActiveRecord::Migration[5.2]
       t.string :postseason
       t.string :score
       t.integer :year
+      t.string :home_team_top_scorers
+      t.string :visitor_team_top_scorers
+      t.integer :point_value 
     end
   end
 end
+# class CreateGames < ActiveRecord::Migration[5.2] jt version 
+#   def change
+#     create_table :games do |t|
+#       t.string :home_team
+#       t.string :visitor_team
+#       t.string :winner
+#       t.string :loser
+#       t.integer :home_team_score
+#       t.integer :visitor_team_score
+#       t.string :score
+#       t.integer :year
+#       t.string :home_team_top_scorers
+#       t.string :visitor_team_top_scorers
+#       t.integer :point_value 
+#     end
+#   end
+# end
