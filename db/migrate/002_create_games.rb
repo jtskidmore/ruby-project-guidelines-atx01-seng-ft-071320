b5dccaf98518298061228555063a11b11c17c3dc
Ceddyp19 <ceddyp117@gmail.com>
2020-07-30 13:40:07 -0500

    updated games table again

--- db/migrate/002_create_games.rb
+++ db/migrate/002_create_games.rb
@@ -4,12 +4,11 @@ class CreateGames < ActiveRecord::Migration[5.2]
   def change
     create_table :games do |t|
       t.string :home_team
-      t.string :away_team
+      t.string :visitor_team
       t.string :winner
       t.string :loser
       t.integer :home_team_score
       t.integer :visitor_team_score
-      t.string :postseason
       t.string :score
       t.integer :year
       t.string :home_team_top_scorers
