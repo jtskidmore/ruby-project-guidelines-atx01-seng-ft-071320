0d6186f9f2c21c27bb686992d571bea19996e9f2
Ceddyp19 <ceddyp117@gmail.com>
2020-07-30 15:26:12 -0500

    The Mona Lisa(finished product)

--- db/migrate/002_create_games.rb
+++ db/migrate/002_create_games.rb
@@ -17,20 +17,3 @@ class CreateGames < ActiveRecord::Migration[5.2]
     end
   end
 end
-# class CreateGames < ActiveRecord::Migration[5.2] jt version 
-#   def change
-#     create_table :games do |t|
-#       t.string :home_team
-#       t.string :visitor_team
-#       t.string :winner
-#       t.string :loser
-#       t.integer :home_team_score
-#       t.integer :visitor_team_score
-#       t.string :score
-#       t.integer :year
-#       t.string :home_team_top_scorers
-#       t.string :visitor_team_top_scorers
-#       t.integer :point_value 
-#     end
-#   end
-# end
--- lib/controller.rb
+++ lib/controller.rb
@@ -25,7 +25,7 @@ class Controller
           when 'Stats'      
             stats
           when 'Help'       
-            instructions
+            help 
           when 'LogOut'    
             @@current_user = nil
             main_menu
@@ -103,8 +103,8 @@ class Controller
     def self.help 
       instructions
       prompt = PROMPT
-      options = prompt.select("\n\n", %w(StartGame MainMenu ))
-      case options
+      option = prompt.select("\n\n", %w(StartGame MainMenu ))
+      case option
       when 'StartGame'
         new_round
       when 'MainMenu'
@@ -132,13 +132,13 @@ class Controller
           
           puts"                                                                                                                                                                                                                                                                      
                                                                                                                                 
-                                                                                                                                                              
+                                                                                                                                                                                                                                                                      
           _ _ _     _                      _                                                                                     
           | | | |___| |___ ___ _____ ___   | |_ ___                                                                             
           | | | | -_| |  _| . |     | -_|  |  _| . |_ _ _                                                                         
           |_____|___|_|___|___|_|_|_|___|  |_| |___|_|_|_|                                                                        
                                                                                                                                  
-                                                                                              ".colorize(:red).on_white            
+                                                                                                                                              ".colorize(:red).on_white            
           
           puts "***********************".cyan * 7
           puts "
@@ -270,95 +270,3 @@ class Controller
   
 
 end
-
-# class CLI  jt version ^^^^^^^^= copied to my version
-
-#   def self.run
-
-#     puts "Welcome, human."
-
-#     puts "This is where the instructions will be."
-#     puts "--------------------"
-
-#     while true
-
-#       puts "Please enter your name or type 'exit' to exit application: "
-#       name = gets.chomp
-#       break if name == "exit"
-#       user = User.find_or_create_by(name: name)
-
-#       puts "Welcome, #{user.name}."
-#       puts "--------------------"
-
-
-#       while true
-#         puts "Press 1 to start new round"
-#         puts "Press 2 to check stats"
-#         puts "--------------------"
-#         select = gets.chomp.to_i
-#         break if select == 1
-#         puts "You have played #{user.round_count} rounds."
-#         puts "You have #{user.win_count} wins and #{user.loss_count} losses."
-#         puts "You have #{user.points} points."^^^^^^^^^^^^^^^^^^^^^^
-#         puts "--------------------"
-#         puts "--------------------"
-#       end
-
-#       new_game = Game.create_game
-
-#       puts "--------------------"
-#       puts "This is a #{new_game.point_value} point question."
-#       puts "The year is #{new_game.year}." #1990 is the api_year_value
-#       puts "The home team was the #{new_game.home_team}, and the away team was the #{new_game.visitor_team}." #api_home_team and #api_away_team
-#       puts "The final score was #{new_game.score}." #api_home_team_points and api_away_team_points
-#       puts "The top scorers for the #{new_game.home_team} were: #{new_game.home_team_top_scorers}."^^^^^^^^^^^^^
-#       puts "The top scorers for the #{new_game.visitor_team} were: #{new_game.visitor_team_top_scorers}."^^^^^^^^^^^^^
-#       puts "--------------------"
-#       puts "Who do you think won?"
-#       puts "--------------------"
-
-#       while true
-#         puts "Type your answer or type 'help' for instructions."
-#         puts "--------------------"
-#         winner = new_game.winner #winner is Warriors if api_home_team_points > api_away_team_points
-#         loser = new_game.loser
-#         input = gets.chomp
-
-#         break if input == "quit"
-
-#         if input == winner
-#           puts "You guessed correctly!"
-#           puts "--------------------"
-#           puts "--------------------"
-#           new_game.save
-#           Round.create_round(user.id, new_game.id, true)
-#           break
-#         elsif input == loser
-#           puts "--------------------"
-#           puts "Incorrect."
-#           puts "Better luck next time!"
-#           puts "--------------------"
-#           puts "--------------------"
-#           new_game.save
-#           Round.create_round(user.id, new_game.id, false)
-
-#           break
-#         elsif input == "help"
-#           puts "This is where the instructions will be."
-#           puts "--------------------"
-#         elsif
-#           puts "Invalid input. Please try again."
-#           puts "--------------------"
-#         end
-
-
-#       end
-
-
-#       end
-
-
-#   end
-
-
-# end
