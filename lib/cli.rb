35cfb5b2eb042965770d26ed0720af404a792f43
Ceddyp19 <ceddyp117@gmail.com>
2020-07-29 22:01:14 -0500

    Almost

--- lib/cli.rb
+++ lib/cli.rb
@@ -17,13 +17,15 @@ class CLI
 
   
     while true
+      name = "name".green 
       exit = "exit".red 
-      puts "Please enter your name or type #{exit} to exit application: "
+      puts "\n" * 2 
+      puts "Please enter your #{name} or type #{exit} to leave application: "
       name = gets.chomp
       break if name == "exit"
-      user = User.find_or_create_by(name: name)
-
-      puts "Welcome, #{user.name}."
+      @@user = User.find_or_create_by(name: name)
+      puts"\n" * 2
+      puts "Welcome, #{@@user.name}."
       puts "--------------------"
 
 
@@ -32,22 +34,25 @@ class CLI
         puts "Press 2 to check stats"
         select = gets.chomp.to_i
         break if select == 1
-        puts "You have played #{user.round_count} rounds."
-        puts "You have #{user.win_count} wins and #{user.loss_count} losses"
+        puts "You have played #{@@user.round_count} round(s)."
+        puts "You have #{@@user.win_count} wins and #{@@user.loss_count} losses"
         puts "--------------------"
         puts "--------------------"
       end
+      play_a_round
+    end #experimental
+     play_a_round
 
-      new_game = Game.create_game
+     def self.play_a_round #experimental code
+        new_game = Game.create_game
+        puts "The year is #{new_game.year}" #1990 is the api_year_value
+        puts "The home team was the #{new_game.home_team}, and the away team was the #{new_game.away_team}" #api_home_team and #api_away_team
+        puts "The final score was #{new_game.score}" #api_home_team_points and api_away_team_points
+        puts "Who do you think won?"
+        puts "--------------------"
+        help = "help".yellow
 
-      puts "The year is #{new_game.year}" #1990 is the api_year_value
-      puts "The home team was the #{new_game.home_team}, and the away team was the #{new_game.away_team}" #api_home_team and #api_away_team
-      puts "The final score was #{new_game.score}" #api_home_team_points and api_away_team_points
-      puts "Who do you think won?"
-      puts "--------------------"
-      help = "help".yellow
-
-      while true
+       while true
         puts "Type your answer or type #{help} for instructions."
         puts "--------------------"
         winner = new_game.winner #winner is Warriors if api_home_team_points > api_away_team_points
@@ -61,15 +66,18 @@ class CLI
           puts "--------------------"
           puts "--------------------"
           new_game.save
-          Round.create_round(user.id, new_game.id, true)
+          Round.create_round(@@user.id, new_game.id, true)
+          prompt = PROMPT#experimental code
+          prompt.select("",%w(Play_Again Main_Menu))#experimental code
           break
         elsif input == loser
           puts "Wrong!!! Do your homework!"
           puts "--------------------"
           puts "--------------------"
           new_game.save
-          Round.create_round(user.id, new_game.id, false)
-
+          Round.create_round(@@user.id, new_game.id, false)
+          prompt = PROMPT#experimental code
+          prompt.select("",%w(Play_Again Main_Menu))#experimental code
           break
         elsif input == "help"
           instructions
@@ -80,11 +88,11 @@ class CLI
         end
 
 
-      end
+      end #experimental code
 
 
       end
-
+    # end #experimental code
 
   end
 
@@ -92,7 +100,7 @@ class CLI
   def self.instructions
 
    puts "WELCOME TO NBA TRIVIA!!!".blue.underline
-   puts "The rules are simple:".green
+   puts "The rules are simple:".white 
    puts "\n" 
    puts "We give you some info about a specific NBA game and you tell us who you think won the game?".green
    puts "Easy enough right?".green
@@ -109,16 +117,16 @@ class CLI
      end
    end
 
-   def self.main
-    puts "\n" * 30
-    if @@current_user == nil
-        puts "You have to be logged in to use IceBreaker :("
-        IceBreaker.login
-    else
-    IceBreaker.banner_icebreaker
-    puts "Hello #{@@current_user.username.light_yellow.bold}!"
-    puts "\n" 
-    menu_selection = PROMPT.select("Select from the following options?", %w(MyIceBreakers RandomIcebreaker DateIcebreaker YearIcebreaker EditMyInfo LogOut))
+  #  def self.main
+  #   puts "\n" * 30
+  #   if @@user == nil
+  #       puts "You have to be logged in to play NBA TRIVIA!!! :("
+  #       IceBreaker.login
+  #   else
+  #   IceBreaker.banner_icebreaker
+  #   puts "Hello #{@@current_user.username.light_yellow.bold}!"
+  #   puts "\n" 
+  #   menu_selection = PROMPT.select("Select from the following options?", %w(MyIceBreakers RandomIcebreaker DateIcebreaker YearIcebreaker EditMyInfo LogOut))
 
 
 
---
+++ lib/cli2.rb
@@ -0,0 +1,222 @@
+class Controller
+  ######################################### Method that runs all other methods ###############################
+ def self.run
+  intro 
+  profile 
+  main_menu
+ end
+
+
+
+ ############################################ ALL OF THE METHODS ###########################################
+
+
+    def self.main_menu 
+     puts "\n"
+     if @@current_user == nil
+            puts "You have to be logged in to play NBA TRIVIA!!! :("
+            login
+     else
+        intro
+        puts "Hello #{@@current_user.name.light_blue.bold}!"
+        puts "\n" 
+        menu_selection = PROMPT.select("Select an option:", %w(StartNewRound Stats Help LogOut QuitGame))
+        case menu_selection
+          when 'StartNewRound' #Need to bring method over from old cli.rb
+            new_round
+          when 'Stats'      
+            stats
+          when 'Help'       
+            instructions
+          when 'LogOut'    
+            @@current_user = nil
+            main_menu
+          when 'Quitgame'
+            quit 
+          end
+    end
+
+    def self.signup
+      sign_up = PROMPT.select("SignUP or Exit", %w(SignUP Exit))
+        
+        if sign_up == "SignUP"
+          puts "Please enter a username:"
+          name = gets.chomp
+            @@current_user = User.create(name: name)
+            main_menu
+        else sign_up == "Exit"
+            quit
+        end
+
+    end
+
+    def self.login
+       login_or_exit = PROMPT.select("", %w(Login ExitGame))
+       if login_or_exit == "Login"
+          puts "\n" * 5
+          find_user = PROMPT.ask("What is your username?".light_cyan, required: true)
+          @@current_user = User.find_by(name: find_user)
+            if @@current_user == nil
+              puts "User not found".light_red
+              puts "Please try again!"
+              self.login
+            end
+        elsif 'ExitGame'
+              quit
+        end
+    end
+
+    def self.profile
+        prompt = PROMPT 
+        y_or_n = prompt.yes?('Do you have a profile?')
+
+        if y_or_n == true
+          login 
+        elsif y_or_n == false
+          new_user
+        end
+    end
+
+    def self.instructions
+
+        puts "WELCOME TO NBA TRIVIA!!!".blue.underline
+        puts "The rules are simple:".white 
+        puts "\n" 
+        puts "We give you some info about a specific NBA game and you tell us who you think won the game?".green
+        puts "Easy enough right?".green
+        puts "Guess correctly and prove your knowledge wizardry! Lets Begin!!!".green
+    end
+ 
+    def self.new_user
+      instructions
+      puts "/".red * 50
+      prompt = PROMPT
+      login = prompt.select("CreateUsername or Exit", %w(CreateUsername ExitGame))
+      if login == 'CreateUsername'
+        signup
+      elsif login == 'ExitGame'
+        quit
+      end
+    end
+
+    def self.intro
+          # puts              "WELCOME TO".colorize(:light_cyan).bold
+          puts"                                                                                                                                                                                                     
+                                                                                              
+                                                                                                                                                              
+          _ _ _     _                      _                                          
+          | | | |___| |___ ___ _____ ___   | |_ ___                                         
+          | | | | -_| |  _| . |     | -_|  |  _| . |_ _ _                    
+          |_____|___|_|___|___|_|_|_|___|  |_| |___|_|_|_|
+                                                          
+                                                                                              ".colorize(:red).on_white     
+          
+          puts "***********************".cyan * 7
+          puts "
+          ███╗   ██╗██████╗  █████╗     ████████╗██████╗ ██╗██╗   ██╗██╗ █████╗     ██╗██╗██╗
+          ████╗  ██║██╔══██╗██╔══██╗    ╚══██╔══╝██╔══██╗██║██║   ██║██║██╔══██╗    ██║██║██║
+          ██╔██╗ ██║██████╔╝███████║       ██║   ██████╔╝██║██║   ██║██║███████║    ██║██║██║
+          ██║╚██╗██║██╔══██╗██╔══██║       ██║   ██╔══██╗██║╚██╗ ██╔╝██║██╔══██║    ╚═╝╚═╝╚═╝
+          ██║ ╚████║██████╔╝██║  ██║       ██║   ██║  ██║██║ ╚████╔╝ ██║██║  ██║    ██╗██╗██╗
+          ╚═╝  ╚═══╝╚═════╝ ╚═╝  ╚═╝       ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═══╝  ╚═╝╚═╝  ╚═╝    ╚═╝╚═╝╚═╝
+                                                                                                      ".colorize(:color => :green, :background => :light_grey)
+                                                                                                  
+    end
+
+    def self.quit
+      puts "\n" * 20
+      print "\s" * 25,"Play Again Soon!".blue
+      puts "\n"
+      puts "                                                
+      ██████╗  ██████╗  ██████╗ ██████╗ ██████╗ ██╗   ██╗███████╗
+      ██╔════╝ ██╔═══██╗██╔═══██╗██╔══██╗██╔══██╗╚██╗ ██╔╝██╔════╝
+      ██║  ███╗██║   ██║██║   ██║██║  ██║██████╔╝ ╚████╔╝ █████╗  
+      ██║   ██║██║   ██║██║   ██║██║  ██║██╔══██╗  ╚██╔╝  ██╔══╝  
+      ╚██████╔╝╚██████╔╝╚██████╔╝██████╔╝██████╔╝   ██║   ███████╗
+      ╚═════╝  ╚═════╝  ╚═════╝ ╚═════╝ ╚═════╝    ╚═╝   ╚══════╝
+                                      ".green.bold
+      print "\s" * 25,"Play Again Soon!".blue
+      puts "\n" * 10             
+  end 
+
+  def stats 
+          puts "You have played #{@@current_user.round_count} round(s)."
+          puts "Wins: #{@@current_user.win_count}" 
+          puts "Losses: #{@@current_user.loss_count}"
+          puts "--------------------" 
+          puts "\n"
+          prompt = PROMPT
+          option = prompt.select("", %w(StartGame MainMenu ExitGame))
+          case option
+          when 'StartGame'
+            new_round
+          when 'MainMenu'
+            main_menu
+          when 'ExitGame'
+            quit
+          end
+
+          puts "--------------------"
+  end
+
+  def new_round
+    new_game = Game.create_game
+    puts "The year is #{new_game.year}" #1990 is the api_year_value
+    puts "The home team was the #{new_game.home_team}, and the away team was the #{new_game.away_team}" #api_home_team and #api_away_team
+    puts "The final score was #{new_game.score}" #api_home_team_points and api_away_team_points
+    puts "Who do you think won?"
+    puts "--------------------"
+    help = "help".yellow
+
+   
+    puts "Type your answer or type #{help} for instructions."
+    puts "--------------------"
+    winner = new_game.winner #winner is Warriors if api_home_team_points > api_away_team_points
+    loser = new_game.loser
+    input = gets.chomp
+
+
+    if input == winner
+      puts "You guessed correctly!"
+      puts "--------------------"
+      puts "--------------------"
+      new_game.save
+      Round.create_round(@@user.id, new_game.id, true)
+      prompt = PROMPT#experimental code
+      option = prompt.select("",%w(Play_Again Main_Menu))#experimental code
+      case option 
+      when 'Play_Again'
+        new_round
+      when 'Main_Menu'
+        main_menu
+      end
+       
+    elsif input == loser
+      puts "Wrong!!! Do your homework!"
+      puts "--------------------"
+      puts "--------------------"
+      new_game.save
+      Round.create_round(@@user.id, new_game.id, false)
+      prompt = PROMPT#experimental code
+      prompt.select("",%w(Play_Again Main_Menu))#experimental code
+        case option 
+          when 'Play_Again'
+            new_round
+          when 'Main_Menu'
+            main_menu
+        end
+    elsif input == "help"
+      instructions
+      puts "--------------------"
+    elsif
+      puts "Invalid input. Please try again."
+      puts "--------------------"
+    end
+
+
+  end
+
+
+  
+
+end
